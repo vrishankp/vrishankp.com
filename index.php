290d6f5aed9c32c7473c899dabc206c1e2a45ace
vrishankp <realvrishankp@gmail.com>
2023-04-01 23:56:44 -0500

    fixed grammar

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
         </div>
         <div class="col-sm-6" style="margin-top: 14%; margin-bottom: 15%">
             <h2>Welcome to my Website!</h2>
-            <p>Hello! My name is Vrishank Paladugu and I am currently a second year at the University of Wisconsin - Madison. 
+            <p>Hello! My name is Vrishank Paladugu, and I am currently a second year at the University of Wisconsin - Madison.
                 I am currently dual majoring with a BS in Computer Science and a BS in Data Science. I will be graduating in May 2025.</p>
             <p>This website was entirely coded by me, Vrishank Paladugu, using HTML5, CSS, Bootstrap, and JavaScript for the frontend and PHP and SQL for the backend.</p>
             <p>This website shows some of my accomplishments and my skills in programming that I hope will impress you!</p>
