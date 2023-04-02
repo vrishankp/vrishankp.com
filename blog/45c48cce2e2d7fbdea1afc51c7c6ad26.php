a4563999efa34187007db3d5478634997f21ad13
Vrishank Paladugu <realvrishankp@gmail.com>
2023-04-02 07:23:41 +0000

    Added new blog

---
+++ blog/45c48cce2e2d7fbdea1afc51c7c6ad26.php
@@ -0,0 +1,19 @@
+
+<?php include 'header.php';?>
+    <div class = 'row'>
+        <div class = 'col-sm-2'>
+        </div>
+        <div class = 'col-sm-8'>
+            <h2>Update to Website</h2>
+            <b>2023-04-02 01:54</b>
+            <br>
+            <p>So pretty big updates to the website:
+1. It is now linked directly to GitHub, so all the changes will be live and can be seen on GitHub.
+2. A new, stronger, hashing algorithm is used to store passwords in the database. This greatly reduces the likelihood of a password leaking as a result of a data breach.
+
+More updates coming soon (hopefully)</p>
+        </div>
+        <div class = 'col-sm-2'>
+        </div>
+    </div>
+<?php include 'footer.php';?>
