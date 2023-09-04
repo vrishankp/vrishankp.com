fa23dad4062839d5caf7e40675de3388cad5db24
vrishankp <realvrishankp@gmail.com>
2023-09-03 19:32:32 -0500

    Final error fix for links on gallery

--- gallery.php
+++ gallery.php
@@ -53,17 +53,19 @@ include 'header.php';
             </div>
         </div>
         <div class = "col-sm-5" style = "padding-left: 5px !important; padding-bottom: 5px; padding-top: 5px;">
-            <a href = "gallery/71010026.jpg" target="_blank">
-                <img src = "gallery/71010026.jpg" class = "image" alt="fountain" style = "width: 100%; height: auto;">
-            </a>
-            <div class="middle">
-                <div class="text"> </div>
-            </div>
-            <div class="second-middle">
-                <div class="second-text">
-                    Mamiya / Sekor 500 DTL <br>
-                    Mamiya / Sekor 50mm lens <br>
-                    Fujifilm 400
+            <div class = "cont">
+                <a href = "gallery/71010026.jpg" target="_blank">
+                    <img src = "gallery/71010026.jpg" class = "image" alt="fountain" style = "width: 100%; height: auto;">
+                </a>
+                <div class="middle">
+                    <div class="text"> </div>
+                </div>
+                <div class="second-middle">
+                    <div class="second-text">
+                        Mamiya / Sekor 500 DTL <br>
+                        Mamiya / Sekor 50mm lens <br>
+                        Fujifilm 400
+                    </div>
                 </div>
             </div>
         </div>
@@ -73,19 +75,21 @@ include 'header.php';
     <div class = "row">
         <div class = "col-sm-1" style = "padding: 0px !important;"></div>
         <div class = "col-sm-10">
-            <div align = "center" class = "bw" style = "padding-bottom: 5px; padding-top: 5px;">
-                <a href = "gallery/71010010.jpg" target="_blank">
-                    <img src = "gallery/71010010.jpg" class = "image" alt="bw" style = "max-width: 100%; height: auto; vertical-align: middle;">
-                </a>
-            </div>
-            <div class="middle">
-                <div class="text"> </div>
-            </div>
-            <div class="second-middle">
-                <div class="second-text">
-                    Mamiya / Sekor 500 DTL <br>
-                    Mamiya / Sekor 50mm lens <br>
-                    Fujifilm 400
+            <div class = "cont">
+                <div align = "center" class = "bw" style = "padding-bottom: 5px; padding-top: 5px;">
+                    <a href = "gallery/71010010.jpg" target="_blank">
+                        <img src = "gallery/71010010.jpg" class = "image" alt="bw" style = "max-width: 100%; height: auto; vertical-align: middle;">
+                    </a>
+                </div>
+                <div class="middle">
+                    <div class="text"> </div>
+                </div>
+                <div class="second-middle">
+                    <div class="second-text">
+                        Mamiya / Sekor 500 DTL <br>
+                        Mamiya / Sekor 50mm lens <br>
+                        Fujifilm 400
+                    </div>
                 </div>
             </div>
         </div>
