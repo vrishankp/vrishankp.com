dda1f5917cd444552869f9640cd34a9f2d8c22a1
vrishankp <realvrishankp@gmail.com>
2024-04-03 23:46:59 -0500

    Made images smaller to improve load times

--- gallery.php
+++ gallery.php
@@ -14,8 +14,8 @@ include 'header.php';
             </center>
 
             <div align = "center" class = "cont" style = "padding-bottom: 5px; padding-top: 5px;">
-                <a href = "gallery/71010028.jpg" target="_blank">
-                    <img src = "gallery/71010028.jpg" alt="tower" class = "image" style = "max-width: 100%; height: auto; vertical-align: middle;">
+                <a href = "gallery/tower.jpeg" target="_blank">
+                    <img src = "gallery/small_tower.jpg" alt="tower" class = "image" style = "max-width: 100%; height: auto; vertical-align: middle;">
                 </a>
                 <div class="middle">
                     <div class="text"> </div>
@@ -36,8 +36,8 @@ include 'header.php';
         <div class = "col-sm-1" style = "padding: 0px !important;"></div>
         <div class = "col-sm-5" style = "padding-right: 5px !important; padding-bottom: 5px; padding-top: 5px;">
             <div class = "cont">
-                <a href = "gallery/71010004.jpg" target="_blank">
-                    <img src = "gallery/71010004.jpg" class = "image" alt="basketball" style = "width: 100%; height: auto;">
+                <a href = "gallery/hoop.jpg" target="_blank">
+                    <img src = "gallery/small_hoop.jpg" class = "image" alt="basketball" style = "width: 100%; height: auto;">
                 </a>
                 <div class="middle">
                     <div class="text"> </div>
@@ -54,8 +54,8 @@ include 'header.php';
         </div>
         <div class = "col-sm-5" style = "padding-left: 5px !important; padding-bottom: 5px; padding-top: 5px;">
             <div class = "cont">
-                <a href = "gallery/71010026.jpg" target="_blank">
-                    <img src = "gallery/71010026.jpg" class = "image" alt="fountain" style = "width: 100%; height: auto;">
+                <a href = "gallery/fountain.jpeg" target="_blank">
+                    <img src = "gallery/small_fountain.jpg" class = "image" alt="fountain" style = "width: 100%; height: auto;">
                 </a>
                 <div class="middle">
                     <div class="text"> </div>
@@ -77,8 +77,8 @@ include 'header.php';
         <div class = "col-sm-10">
             <div class = "cont">
                 <div align = "center" class = "bw" style = "padding-bottom: 5px; padding-top: 5px;">
-                    <a href = "gallery/71010010.jpg" target="_blank">
-                        <img src = "gallery/71010010.jpg" class = "image" alt="bw" style = "max-width: 100%; height: auto; vertical-align: middle;">
+                    <a href = "gallery/bench.jpg" target="_blank">
+                        <img src = "gallery/small_bench.jpg" class = "image" alt="bench" style = "max-width: 100%; height: auto; vertical-align: middle;">
                     </a>
                 </div>
                 <div class="middle">
@@ -95,6 +95,70 @@ include 'header.php';
         </div>
         <div class = "col-sm-1" style = "padding: 0px !important;"></div>
     </div>
+    <div class = "row">
+        <div class = "col-sm-1" style = "padding: 0px !important;"></div>
+        <div class = "col-sm-5" style = "padding-right: 5px !important; padding-bottom: 5px; padding-top: 5px;">
+            <div class = "cont">
+                <a href = "gallery/stair1.jpg" target="_blank">
+                    <img src = "gallery/small_stair1.jpg" class = "image" alt="stair1" style = "width: 100%; height: auto;">
+                </a>
+                <div class="middle">
+                    <div class="text"> </div>
+                </div>
+
+                <div class="second-middle">
+                    <div class="second-text">
+                        Mamiya / Sekor 500 DTL <br>
+                        Mamiya / Sekor 50mm lens <br>
+                        Fujifilm 400
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class = "col-sm-5" style = "padding-left: 5px !important; padding-bottom: 5px; padding-top: 5px;">
+            <div class = "cont">
+                <a href = "gallery/stair2.jpeg" target="_blank">
+                    <img src = "gallery/small_stair2.jpg" class = "image" alt="stair2" style = "width: 100%; height: auto;">
+                </a>
+                <div class="middle">
+                    <div class="text"> </div>
+                </div>
+                <div class="second-middle">
+                    <div class="second-text">
+                        Mamiya / Sekor 500 DTL <br>
+                        Mamiya / Sekor 50mm lens <br>
+                        Fujifilm 400
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class = "col-sm-1" style = "padding: 0px !important;" ></div>
+    </div>
+    <div class = "row">
+        <div class = "col-sm-1" style = "padding: 0px !important;"></div>
+        <div class = "col-sm-10">
+            <div class = "cont">
+                <div align = "center" class = "bw" style = "padding-bottom: 5px; padding-top: 5px;">
+                    <a href = "gallery/dock.jpg" target="_blank">
+                        <img src = "gallery/small_dock.jpg" class = "image" alt="dock" style = "max-width: 100%; height: auto; vertical-align: middle;">
+                    </a>
+                </div>
+                <div class="middle">
+                    <div class="text"> </div>
+                </div>
+                <div class="second-middle">
+                    <div class="second-text">
+                        Mamiya / Sekor 500 DTL <br>
+                        Mamiya / Sekor 50mm lens <br>
+                        Fujifilm 400
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class = "col-sm-1" style = "padding: 0px !important;"></div>
+    </div>
+
 <?php
 include 'footer.php';
 ?>
