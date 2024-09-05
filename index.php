247216e150b79cd5274cae2aa0290b9180a037a1
vrishankp <realvrishankp@gmail.com>
2024-09-05 13:56:05 -0500

    Updated for senior year

--- index.php
+++ index.php
@@ -8,11 +8,12 @@
         </div>
         <div class="col-sm-6" style="margin-top: 14%; margin-bottom: 15%">
             <h2>Welcome to my Website!</h2>
-            <p>Hello! My name is Vrishank Paladugu, and I am currently a third year at the University of Wisconsin - Madison.
+            <p>Hello! My name is Vrishank Paladugu, and I am currently a senior at the University of Wisconsin - Madison.
                 I am currently dual majoring with a BS in Computer Science and a BS in Data Science. I will be graduating in May 2025.</p>
             <p>This website was entirely coded by me, Vrishank Paladugu, using HTML5, CSS, Bootstrap, and JavaScript for the frontend and PHP and SQL for the backend.</p>
             <p>This website shows some of my accomplishments and my skills in programming that I hope will impress you!</p>
-            <p>Access the code for this website on <a href="https://github.com/vrishankp/vrishankp.com" target="_blank">GitHub</a></p>
+            <p> I am looking for any full time Software Engineering or Data Science roles once I graduate, so please feel free to contact me if you are hiring.</p>
+            <p>Thank you for visiting my website! You can access the code for this website on <a href="https://github.com/vrishankp/vrishankp.com" target="_blank">GitHub</a></p>
         </div>
     </div>
 
